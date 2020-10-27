013cfd43b4e6d08b0ed2734fc65a9fb5e8cf9c0b
Anson <1072476988@qq.com>
2020-10-27 22:01:20 +0800

    [Lingzhen Kong] fix: use deep copy and not remove origin data.

--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/service/StudentService.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/service/StudentService.java
@@ -18,16 +18,17 @@ public class StudentService {
 
     public List<Map<Integer, String>> grouping() {
         final Map<Integer, String> allStudent = studentDao.getAllStudent();
+        final Map<Integer, String> allStudent2 = new HashMap<>(allStudent);
         final int size = allStudent.size();
         final ArrayList<Map<Integer, String>> maps = new ArrayList<>();
         for (int i = 0; i < 6; i++) {
             maps.add(new HashMap<>());
         }
         for (int i = 0; i < size; i++) {
-            Integer[] keys = allStudent.keySet().toArray(new Integer[0]);
+            Integer[] keys = allStudent2.keySet().toArray(new Integer[0]);
             Random random = new Random();
             Integer randomKey = keys[random.nextInt(keys.length)];
-            final String remove = allStudent.remove(randomKey);
+            final String remove = allStudent2.remove(randomKey);
             maps.get(i % 6).put(randomKey, remove);
         }
         System.out.println(maps);
